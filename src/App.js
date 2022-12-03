5018032c27d4087c1c6d2010b887e1d3492c5b69
ferutizi <fer.utizi@gmail.com>
2022-12-03 14:36:43 -0300

    Día 3: Input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 import './index.css';
+import { useState } from 'react';
 
 function App() {
+  const [formulario, setFormulario] = useState({regalo: ''});
+  const [regalos, setRegalos] = useState([]);
+
+  const handleChange = (e) => {
+    setFormulario({
+      ...formulario,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setRegalos([
+      ...regalos,
+      formulario.regalo
+    ]);
+    setFormulario({regalo: ''})
+  }
+
   return (
     <>
       <div className="container">
-        <h1 className='title__list'>Lista de regalos</h1>
-        <ul>
-          <li className="item__list">Medias</li>
-          <li className="item__list">Cuerdas de guitarra</li>
-          <li className="item__list">Teclado</li>
-        </ul>
+        <h1>Lista de regalos</h1>
+        <form onSubmit={handleSubmit}>
+          <input 
+            name="regalo"
+            type="text"
+            onChange={handleChange}
+            value={formulario.regalo}
+            autoFocus
+          />
+          <button>Agregar</button>
+        </form>
+          <ul>
+            {
+              regalos.map(item => (
+                <li>
+                  <div className="item__container">
+                    <p className="item">{item}</p>
+                  </div>
+                </li>
+                ))
+            }
+          </ul>
       </div>
     </>
   );
