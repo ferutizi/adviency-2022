3144f29c9aa3dde57f8dc0fb580f1152a2e47ebf
ferutizi <fer.utizi@gmail.com>
2022-12-04 15:46:12 -0300

    Día 4: clear

--- src/App.js
+++ src/App.js
@@ -1,52 +1,8 @@
-import './index.css';
-import { useState } from 'react';
-
 function App() {
-  const [formulario, setFormulario] = useState({regalo: ''});
-  const [regalos, setRegalos] = useState([]);
-
-  const handleChange = (e) => {
-    setFormulario({
-      ...formulario,
-      [e.target.name]: e.target.value
-    })
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setRegalos([
-      ...regalos,
-      formulario.regalo
-    ]);
-    setFormulario({regalo: ''})
-  }
 
   return (
     <>
-      <div className="container">
-        <h1>Lista de regalos</h1>
-        <form onSubmit={handleSubmit}>
-          <input 
-            name="regalo"
-            type="text"
-            onChange={handleChange}
-            value={formulario.regalo}
-            autoFocus
-          />
-          <button>Agregar</button>
-        </form>
-          <ul>
-            {
-              regalos.map(item => (
-                <li>
-                  <div className="item__container">
-                    <p className="item">{item}</p>
-                  </div>
-                </li>
-                ))
-            }
-          </ul>
-      </div>
+      
     </>
   );
 }
