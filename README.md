f67d97e0adc8adb1a0120d6863a4855c3407bbb5
ferutizi <fer.utizi@gmail.com>
2022-12-08 18:18:17 -0300

    Día 8: Boton de sumar y restar unidades

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ La aplicacion localmente corre en el puerto 3000
 
 - [x] Día 7: Tuvimos algunos reportes de regalos vacíos o repetidos, asegurmosnos que la gente solo pueda agregar un regalo si escribió algo y si ese regalo no está ya en la lista!
 
-- [ ] Día 8: Cometimos un error el día anterior, la gente quiere agregar regalos repetidos para regalarselos a diferentes personas, agreguemos un campo al lado del input de texto para poner la cantidad de unidades del regalo que deberíamos comprar.
+- [x] Día 8: Cometimos un error el día anterior, la gente quiere agregar regalos repetidos para regalarselos a diferentes personas, agreguemos un campo al lado del input de texto para poner la cantidad de unidades del regalo que deberíamos comprar.
 
 - [ ] Día 9: La gente está triste por que al cerrar la aplicación pierde todos sus regalos 😢. Usemos `localStorage` para guardar los regalos en el dispositivo del usuario y cargarlos cuando vuelve!
 
--- src/components/Gifts.jsx
+++ src/components/Gifts.jsx
@@ -7,13 +7,33 @@ const Gifts = ({ gifts, setGifts }) => {
         setGifts(newGifts);
     }
 
+    const aumentar = (gift) => {
+        if(gift.count >= 0) {
+            gifts.filter(item => item.name === gift.name).map(item => item.count = item.count + 1);
+            setGifts([...gifts]);
+        }
+    }
+
+    const disminuir = (gift) => {
+        if(gift.count > 0) {
+            gifts.filter(item => item.name === gift.name).map(item => item.count = item.count - 1);
+            setGifts([...gifts]);
+        }
+    }
+
     return(
         <div className="gift__list items">
             {gifts != '' ?
                 gifts.map(item => 
                     <div key={item.name} className="items__container">
                         <p className="items__details">{item.name} {item.count === 0 ? null : `x${item.count}`}</p>
-                        <button className="items__button" type="button" onClick={() => deleteGift(item.name)}>X</button>
+                        <div className='items__buttons--flex'>
+                            <div>
+                                <button className="items__button" type='button' onClick={() => aumentar(item)}>+</button>
+                                <button className="items__button" type='button' onClick={() => disminuir(item)}>-</button>
+                            </div>
+                            <button className="items__button--red" type="button" onClick={() => deleteGift(item.name)}>x</button>
+                        </div>
                     </div>)
             : <div className='gift__error'>¿No quieres nada para navidad? Vamos... debe haber algo que quieras</div>
             }
