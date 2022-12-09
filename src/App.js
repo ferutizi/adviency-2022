eecc877f719e3a8df32ed2d43b8ffd974cf9e90f
ferutizi <fer.utizi@gmail.com>
2022-12-09 17:18:06 -0300

    custom hook: useLocalStorage

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './index.scss';
 import { useState } from 'react';
 import Formulario from "./components/Formulario";
 import Gifts from "./components/Gifts";
+import { useLocalStorage } from './hooks/useLocalStorage';
 
 function App() {
-  const [gifts, setGifts] = useState([]);
+  const [gifts, setGifts] = useLocalStorage('gifts', []);
 
   return (
     <>
