ec09d5d1957c30e5efc0ab5ad1ea5f31844d87ed
ferutizi <fer.utizi@gmail.com>
2022-12-09 15:27:32 -0300

    Día 9: clear

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-import './index.scss';
-import { useState } from "react";
-import Formulario from "./components/Formulario";
-import Gifts from "./components/Gifts";
-
 function App() {
-  const [gifts, setGifts] = useState([]);
 
   return (
     <>
-      <div className="gift__container">
-        <div>
-          <h1 className="gift__title">Lista de regalos</h1>
-          <Formulario gifts={gifts} setGifts={setGifts} />
-          <Gifts gifts={gifts} setGifts={setGifts} />
-        </div>
-        <button type="button" className="gift__button" onClick={() => setGifts([])}>Quitar todos</button>
-      </div>
     </>
   );
 }
